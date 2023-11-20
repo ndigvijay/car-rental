1639bb68a2b935cf75e8051bfcc7d8b7a322cfb3
Nishanth <nishanthdmello2003@gmail.com>
2023-11-20 17:56:20 +0530

    added css to about us and contact us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 const AboutUs = () => {
   return (
-    <div>
-        <Navbar/>
-        <div className="container mt-5">
-        <h2>About Us</h2>
-        <p>
-            Welcome to XYZ Car Rentals, your go-to destination for quality car rentals. Our mission is to provide
+    <div className='about_us_outer_div'>
+      <Navbar />
+      <div className='about_us_div'>
+        <div className="container mt-5 about_us_text">
+          <h2><b>About Us</b></h2>
+          <br /><br />
+          <p>Welcome to Cruise Connect, your go-to destination for quality car rentals. Our mission is to provide
             customers with a seamless and enjoyable experience while offering a diverse fleet of vehicles to meet
-            your transportation needs.
-        </p>
-        <p>
-            At XYZ Car Rentals, we prioritize customer satisfaction and safety. Our team is dedicated to delivering
-            top-notch service and ensuring that your journey with us is memorable and stress-free.
-        </p>
+            your transportation needs.</p>
+          <br />
+          <p>At Cruise Connect, we prioritize customer satisfaction and safety. Our team is dedicated to delivering
+            top-notch service and ensuring that your journey with us is memorable and stress-free.</p>
         </div>
+        <img className="about_us_image" src="https://www.revv.co.in/blogs/wp-content/uploads/2020/09/Car-Rentals-in-India.jpg" alt="about us image" />
+      </div>
     </div>
   );
 };
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,30 +3,28 @@ import Navbar from '../components/Navbar';
 const ContactUs = () => {
   return (
     <div>
-        <Navbar/>
-        <div className="container mt-5">
-        <h2>Contact Us</h2>
-        <p>
-            If you have any questions, feedback, or inquiries, feel free to reach out to us. Our customer support
-            team is available to assist you.
-        </p>
-        <p>
-            Contact Information:
+      <Navbar />
+      <div className="container mt-5 contact_us_div">
+        <div className="contact_us_text">
+          <h2><b>Contact Us</b></h2>
+          <br /><br />
+          <p>If you have any questions, feedback, or inquiries, feel free to reach out to us. Our customer support team is available to assist you.</p>
+          <br />
+          <p>
+            <b>Email : </b>info@xyzcarrentals.com
             <br />
-            Email: info@xyzcarrentals.com
-            <br />
-            Phone: +1 (555) 123-4567
-        </p>
-        <p>
-            Visit us at our main office:
-            <br />
-            XYZ Car Rentals
+            <b>Phone : </b>+1 (555) 123-4567
+            <br /><br />
+            <b>Cruise Connect Address :</b>
             <br />
             123 Main Street, Cityville, State, 12345
-        </p>
+          </p>
         </div>
+        <img className="about_us_image" src="https://cdn.pixabay.com/photo/2017/12/02/14/38/contact-us-2993000_640.jpg" alt="contact us image" />
+      </div>
     </div>
   );
 };
 
 export default ContactUs;
+
