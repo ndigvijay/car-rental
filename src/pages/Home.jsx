9bf8eb8c0319a527d435a5eaa6c201a1bb110571
ndigvijay <digvijayn1993@gmail.com>
2024-01-09 20:55:57 +0530

    added little mobile responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   useEffect(() => {
     const fetchCars = async () => {
       try {
-        const response = await fetch('https://car-rental-sfqr.onrender.com/app/getcarsHome'); // replace with your actual backend URL
+        const response = await fetch('https://car-rental-sfqr.onrender.com/app/getcarsHome');
         if (response.ok) {
           const data = await response.json();
           setCars(data);
@@ -26,45 +26,40 @@ const Home = () => {
     <div>
       <Navbar />
       <div className="home_page_div">
+        <div>
+          <h1>Welcome to Cruise Connect</h1>
+          <p className="lead">Rent a car for your next adventure!</p>
+          <hr />
+          <p>Explore our premium selection of cars at affordable prices.</p>
+          <br />
+          <br />
+          <a href="/cars" className="btn btn-primary btn-lg">Browse Cars</a>
+        </div>
 
-          <div>
-            <h1>Welcome to Cruise Connect</h1>
-            <p className="lead">Rent a car for your next adventure!</p>
-            <hr/>
-            <p>Explore our premium selection of cars at affordable prices.</p>
-            <br />
-            <br />
-            <a href="/cars" className="btn btn-primary btn-lg">Browse Cars</a>
-          </div>
-
-          <div>
-            <img className='home_img' src="https://pngimg.com/d/bmw_PNG1710.png" alt="car image" />
-            <div className="home_page_inner_div"></div>
-          </div>
-
+        <div>
+          <img className='home_img' src="https://pngimg.com/d/bmw_PNG1710.png" alt="car image" />
+          <div className="home_page_inner_div"></div>
+        </div>
       </div>
+
+      {/* Responsive Car Display */}
+      {/* <div className="row">
+        {cars.map((car) => (
+          <div className="col-md-4 col-sm-6" key={car._id}>
+            <div className="card mb-4">
+              <img src={car.image} className="card-img-top" style={{ height: "280px", width: "auto" }} alt={car.model} />
+              <div className="card-body">
+                <h5 className="card-title">{car.make} {car.model}</h5>
+                <p className="card-text">Year: {car.year}</p>
+                <p className="card-text">Price: {car.price}</p>
+                <p className="card-text">{car.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div> */}
     </div>
   );
 };
 
 export default Home;
-
-
-
-{/* 
-<div className="row">
-  {cars.map((car) => (
-    <div className="col-md-4" key={car._id}>
-      <div className="card mb-4">
-        <img src={car.image} className="card-img-top" style={{ height: "280px", width: "auto" }} alt={car.model} />
-        <div className="card-body">
-          <h5 className="card-title">{car.make} {car.model}</h5>
-          <p className="card-text">Year: {car.year}</p>
-          <p className="card-text">Price: {car.price}</p>
-          <p className="card-text">{car.description}</p>
-        </div>
-      </div>
-    </div>
-  ))}
-</div>
-*/}
